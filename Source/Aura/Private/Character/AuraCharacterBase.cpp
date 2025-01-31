d16f93478b06936d72dd6a246be009ac0d6038cc
KaanKirant <kaankirant@hotmail.com>
2025-01-31 09:44:20 +0000

    Editted the base character class and derived two new classes from this class. AuraCharacter and AuraEnemy.

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright Kaan Kirant
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
