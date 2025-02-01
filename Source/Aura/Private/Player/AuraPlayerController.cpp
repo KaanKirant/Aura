ec8a78fecf080d0142e5661b1962c7ef154df777
KaanKirant <kaankirant@hotmail.com>
2025-02-01 07:52:05 +0000

    Created a subsystem for enhanced input system and added cursor modes.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,32 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	//Replication is for multiplayer. It replicates the changes to other clients.
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//Assert has been used here to make sure player can move. If player cannot move the game should crash out.
+	check(AuraContext);
+	//Setting up the enhanced input system
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	//Crash if subsystem is null.
+	check(Subsystem);
+	//Adding aura mapping context to subsystem.
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	//Setting cursor details.
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
